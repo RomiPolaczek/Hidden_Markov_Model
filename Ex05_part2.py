8fc7352722ad9aa38e43e60998b5d4c88c41da0a
romipolaczek <168196934+romipolaczek@users.noreply.github.com>
2024-05-24 01:45:32 +0300

    final version change recursion to loop

--- Ex05_part2.py
+++ Ex05_part2.py
@@ -33,27 +33,22 @@ data_file.close()
 
 
 def binary_search(left, right):
-    if right - left <= 0.005:
-        return right
-    mid = float(left + right)/2
-    count_smaller_eq_mid = 0
-    with open("DataFile.txt", "r") as data_file:
-        line = data_file.readline()
-        for line in data_file:
-            string_num = line.split()
-            for num in string_num:
-                if mid >= int(num):
-                    count_smaller_eq_mid += 1
-        if float(count_smaller_eq_mid)/count_all_nums >= 0.6:
-            return binary_search(left, mid)
+    while right - left > 0.005:
+        mid = float(left + right) / 2
+        count_smaller_eq_mid = 0
+        with open("DataFile.txt", "r") as data_file:
+            line = data_file.readline()
+            for line in data_file:
+               string_num = line.split()
+               for num in string_num:
+                   if mid >= int(num):
+                       count_smaller_eq_mid += 1
+        if float(count_smaller_eq_mid) / count_all_nums >= 0.6:
+            right = mid
         else:
-            return binary_search(mid, right)
+            left = mid
+    return right
 
 
 percentile60 = binary_search(min, max)
 print("The 60 percentile number is:", percentile60)
-
-
-
-
-
